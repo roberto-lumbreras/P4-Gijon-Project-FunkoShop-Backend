3a718973ecdb8ffcba663736895fc598bb440671
roberto-lumbreras <rlumbrerascanal@gmail.com>
2024-11-26 10:41:56 +0100

    project initialization

---
+++ src/main/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/P4GijonProjectFunkoshopBackendApplication.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.p4_gijon_project_funkoshop_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P4GijonProjectFunkoshopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P4GijonProjectFunkoshopBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/P4GijonProjectFunkoshopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.p4_gijon_project_funkoshop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P4GijonProjectFunkoshopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
