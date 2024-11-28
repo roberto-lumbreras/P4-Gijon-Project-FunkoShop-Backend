620c3057e4eb488ca326de2227e4ffbb6b9e9610
SusanaArtime <susartime@gmail.com>
2024-11-28 15:31:57 +0100

    Fix imports model Category in CategoryController

--- src/main/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/category/CategoryService.java
+++ src/main/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/category/CategoryService.java
@@ -1,8 +1,6 @@
 package org.factoriaf5.p4_gijon_project_funkoshop_backend.category;
 
 import java.util.List;
-import java.util.Locale.Category;
-
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,12 +15,12 @@ public class CategoryService {
         return categoryRepository.findAll().stream() .map(x -> new CategoryDTO(x)).toList();
     }
 
-    public CategoryDTO setCategory (Long categoryId, CategoryDTO categoryDTO) {
+    public CategoryDTO setCategory (CategoryDTO categoryDTO,Long categoryId) {
         Category existingCategory = categoryRepository.findById(categoryId).orElse(null);
         if (existingCategory != null) {
             existingCategory.setName(categoryDTO.getName());
             existingCategory.setImageHash(categoryDTO.getImageHash());
-            existingCategory.setHighLights(categoryDTO.getHighlights());
+            existingCategory.setHighlights(categoryDTO.isHighlights());
             return new CategoryDTO(categoryRepository.save(existingCategory));
         } else {
             throw new RuntimeException("Category not found");
