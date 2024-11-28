83f4491e3c1382c850d9dad9dc52cf9c2bea5fad
roberto-lumbreras <rlumbrerascanal@gmail.com>
2024-11-28 15:15:38 +0100

    uploading unsaved archives

--- src/main/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/category/Category.java
+++ src/main/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/category/Category.java
@@ -1,4 +1,4 @@
-package org.factoriaf5.p4_gijon_project_funkoshop_backend;
+package org.factoriaf5.p4_gijon_project_funkoshop_backend.category;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
--- src/main/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/category/CategoryRepository.java
+++ src/main/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/category/CategoryRepository.java
@@ -1,4 +1,4 @@
-package org.factoriaf5.p4_gijon_project_funkoshop_backend;
+package org.factoriaf5.p4_gijon_project_funkoshop_backend.category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
