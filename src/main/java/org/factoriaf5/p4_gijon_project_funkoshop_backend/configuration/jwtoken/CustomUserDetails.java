0466b7a41729d576f543fa03ba2e293a5cb69280
Acacia-Sanchez-Pastur <sanchezpastur@gmail.com>
2024-12-02 11:16:40 +0100

    added customuserdetails that implement userdetails(java class) for doing register and login with email instead of username

---
+++ src/main/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/configuration/jwtoken/CustomUserDetails.java
@@ -0,0 +1,55 @@
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+
+public class CustomUserDetails implements UserDetails {
+
+    private String email; // Nuevo campo
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public CustomUserDetails(String email, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return email; // Aquí hacemos que el email sea el identificador único
+    }
+
+    public String getEmail() {
+        return email; // Agregamos un getter para obtener el email
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
