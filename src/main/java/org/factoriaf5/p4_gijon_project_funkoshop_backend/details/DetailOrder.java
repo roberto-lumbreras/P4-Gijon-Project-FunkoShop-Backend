b9d5b51e5f0d051dea2037b67c7d03801a75f34c
Rubén Blanco <r.blanco.3d@gmail.com>
2024-12-02 08:44:07 +0100

    Entity  DetailOrder implemented

---
+++ src/main/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/details/DetailOrder.java
@@ -0,0 +1,86 @@
+package org.factoriaf5.p4_gijon_project_funkoshop_backend.details;
+
+import org.factoriaf5.p4_gijon_project_funkoshop_backend.order.Order;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "details")
+public class DetailOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long detailId;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @Column(name = "amount", nullable = false)
+    private Integer amount;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    public DetailOrder(Long id, Product product, Order order, Integer amount, Double price) {
+        this.detailId = id;
+        this.product = product;
+        this.order = order;
+        this.amount = amount;
+        this.price = price;
+    }
+
+    public DetailOrder() {
+    }
+
+    public Long getDetailID() {
+        return detailId;
+    }
+
+    public void setDetailID(Long detailId) {
+        this.detailId = detailId;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
