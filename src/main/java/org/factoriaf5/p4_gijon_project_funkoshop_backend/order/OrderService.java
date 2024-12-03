8b3153e696f122cc640ecb58fa0dad119bfe37f5
Alejandra <siemi121001@gmail.com>
2024-12-03 14:03:59 +0100

    method getStatus in OrderService

--- src/main/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/order/OrderService.java
+++ src/main/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/order/OrderService.java
@@ -1,11 +1,15 @@
 package org.factoriaf5.p4_gijon_project_funkoshop_backend.order;
 
-import java.util.List;
+import java.util.Optional;
 
+import org.apache.catalina.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
+
+    public OrderService() {
+    }
 
     public enum Status {
         PENDING, SHIPPED, DELIVERED, CANCELED
@@ -25,7 +29,32 @@ public class OrderService {
         this.orderRepository = orderRepository;
     }
 
-    public List<Order> getById() {
-        return orderRepository.findById(long);
+
+    public OrderDto getStatus(String authorizationHeader, Long orderId) {
+        String token = authorizationHeader.substring(7);
+        String emailToken = jwtUtil.generateEmailByToken(token);
+        
+        User user = userRepository.findUserByEmail(emailToken);
+        
+        if(user == null) {
+            throw new RuntimeException("User not found.");
+        }
+
+        if (!token.equals(user.getToken())) {
+            throw new RuntimeException("Invalid token.");
+        }
+
+        if (!"user".equals(user.getRoles()) && !"admin".equals(user.getRoles())) {
+            throw new RuntimeException("Unauthorized user.");
+        }
+
+        Optional<Order> order = orderRepository.findById(orderId);
+
+        if (order == null) {
+            throw new RuntimeException("Order not found.");
+        }
+
+        return order.get().getStatus();
     }
+
 }
