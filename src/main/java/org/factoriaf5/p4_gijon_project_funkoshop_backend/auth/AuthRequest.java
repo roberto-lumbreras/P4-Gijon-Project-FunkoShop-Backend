c87ba8af9f61fe5bf7269d924ea1f6316ff603b8
Maria Garcia <mgarciasierragrsr@gmail.com>
2024-12-03 12:48:11 +0100

    Changed name

---
+++ src/main/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/auth/AuthRequest.java
@@ -0,0 +1,24 @@
+package org.factoriaf5.p4_gijon_project_funkoshop_backend.auth;
+
+public class AuthRequest {
+    
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }    
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
