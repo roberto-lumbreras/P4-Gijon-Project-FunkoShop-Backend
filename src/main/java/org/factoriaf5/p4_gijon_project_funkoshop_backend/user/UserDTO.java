c4e60dd9aeb2265652aebb40fbee6b77c4255c9d
Maria Garcia <mgarciasierragrsr@gmail.com>
2024-12-03 12:58:28 +0100

    feat: create Profile.java, Role.java, User.java and UserDTO.java

---
+++ src/main/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/user/UserDTO.java
@@ -0,0 +1,10 @@
+package org.factoriaf5.p4_gijon_project_funkoshop_backend.user;
+
+public class UserDTO {
+    private Long id;
+    private String email;
+    private String password;
+    private Role role;
+    private ProfileDto profile;
+
+}
