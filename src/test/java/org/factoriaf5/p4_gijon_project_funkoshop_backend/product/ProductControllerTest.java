a205c33fdaaf3b819554b05295dc6b77587b6968
Morty1904 <oscarendercraft@gmail.com>
2024-12-04 15:27:18 +0100

    implemented the final tests for the controller of product

--- src/test/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/product/ProductControllerTest.java
+++ src/test/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/product/ProductControllerTest.java
@@ -7,6 +7,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
@@ -25,12 +27,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.math.BigDecimal;
 import java.util.List;
 
+@WebMvcTest(ProductController.class)
 class ProductControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Mock
+    @MockBean
     private ProductService productService;
 
     @InjectMocks
