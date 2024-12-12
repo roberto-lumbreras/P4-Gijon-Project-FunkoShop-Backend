7979ca6271b26de149f148e41a070feef048d69b
abelpriem <abelpriem94@hotmail.com>
2024-12-12 11:18:41 +0100

    modify POM with dependencies

--- src/main/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/user/User.java
+++ src/main/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/user/User.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.factoriaf5.p4_gijon_project_funkoshop_backend.product.Product;
 
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 /* import org.factoriaf5.p4_gijon_project_funkoshop_backend.profile.Profile; */
 /*  */
 //import jakarta.persistence.CascadeType;
@@ -11,6 +14,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 //import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -34,6 +38,10 @@ public class User {
     private Boolean enabled;
     private Role role;
     private String JwToken;
+
+    @ElementCollection
+    @CollectionTable(name = "user_favorites", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "product_id")
     private List<Product> favorites;
     /*
      * @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
