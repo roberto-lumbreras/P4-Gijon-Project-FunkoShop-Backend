5fc56f4f254dfea8788335256f7bd47848b6b71c
Rubén Blanco <r.blanco.3d@gmail.com>
2024-12-16 15:19:04 +0100

    User Controler updated with new profile methods

--- src/main/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/user/UserController.java
+++ src/main/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/user/UserController.java
@@ -118,4 +118,48 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).body(error.getMessage());
         }
     }
+
+    @PostMapping("/user/add-first-address/{userId}")
+    public ResponseEntity<String> addFirstAddress(@RequestHeader("Authorization") String authorizationHeader, @RequestBody String entity) {
+        try {
+            if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+        
+            userService.addFirstAddress(authorizationHeader, entity);
+            return ResponseEntity.status(HttpStatus.OK).body("Address was succesfully saved!");
+        
+        }
+        catch (IllegalArgumentException error) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error.getMessage());
+        } catch (SecurityException error) {
+            return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(error.getMessage());
+        } catch (AccessDeniedException error) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(error.getMessage());
+        }
+    }
+
+    @PatchMapping("/user/add-second-address/{userId}")
+    public String addSecondAddress(@RequestBody String entity) {
+        //TODO: process POST request
+        
+        return entity;
+    }
+
+    @DeleteMapping("/user/remove-first-address/{userId}")
+    public String deleteFirstAddress(@RequestBody String entity) {
+        //TODO: process POST request
+        
+        return entity;
+    }
+
+    @DeleteMapping("/user/remove-second-address/{userId}")
+    public String deleteSecondAddress(@RequestBody String entity) {
+        //TODO: process POST request
+        
+        return entity;
+    }
+
+    
+    
 }
