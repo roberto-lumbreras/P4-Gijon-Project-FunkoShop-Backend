b5585f40621589afd3fa78a4ffa062856b794c05
Acacia-Sanchez-Xanina <43806224+Acacia-Sanchez@users.noreply.github.com>
2024-12-16 15:54:22 +0100

    in merge from TeamOne, AuthTokenFilter was empty, so I'm copying&pasting for solvign bad merged

---
+++ src/main/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/configuration/jwtoken/AuthTokenFilter.java
@@ -0,0 +1,83 @@
+package org.factoriaf5.p4_gijon_project_funkoshop_backend.configuration.jwtoken;
+
+import java.io.IOException;
+
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class AuthTokenFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private DataSource dataSource;
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
+
+    @Override
+    protected void doFilterInternal(@SuppressWarnings("null") HttpServletRequest request,
+            @SuppressWarnings("null") HttpServletResponse response, @SuppressWarnings("null") FilterChain filterChain)
+            throws ServletException, IOException {
+        // logger.debug("AuthTokenFilter called for URI: {}", request.getRequestURI());
+        try {
+            String jwt = parseJwt(request);
+            if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
+                String email = jwtUtils.getEmailFromJwtToken(jwt);
+
+                UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+
+                /*
+                 * logger.debug("Roles from JWT: {}", userDetails.getAuthorities());
+                 * 
+                 * System.out.println(userDetails);
+                 * 
+                 * List<String> roles = userDetails.getAuthorities().stream()
+                 * .map(GrantedAuthority::getAuthority) // Extrae el nombre del rol
+                 * .toList();
+                 * logger.debug("User roles: {}", roles);
+                 * 
+                 * authentication.setDetails(new
+                 * WebAuthenticationDetailsSource().buildDetails(request));
+                 * 
+                 * SecurityContextHolder.getContext().setAuthentication(authentication);
+                 */
+
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+
+        } catch (Exception e) {
+            logger.error("Cannot set user authentication: {}", e);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String parseJwt(HttpServletRequest request) {
+        String jwt = jwtUtils.getJwtFromHeader(request);
+        logger.debug("AuthTokenFilter.java: {}", jwt);
+        return jwt;
+    }
+
+}
