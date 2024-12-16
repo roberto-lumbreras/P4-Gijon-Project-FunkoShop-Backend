d9e9cdc8386dfed53cc7be92ff7f807a4f5c8746
Maria Garcia <mgarciasierragrsr@gmail.com>
2024-12-16 17:48:09 +0100

    more things from the merge

---
+++ src/main/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/configuration/SecurityConfig.java
@@ -0,0 +1,104 @@
+package org.factoriaf5.p4_gijon_project_funkoshop_backend.configuration;
+
+import javax.sql.DataSource;
+
+import org.factoriaf5.p4_gijon_project_funkoshop_backend.configuration.jwtoken.AuthEntryPointJwt;
+import org.factoriaf5.p4_gijon_project_funkoshop_backend.configuration.jwtoken.AuthTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
+    private static final String[] WhiteList = {
+            "/auth/**",
+            "/api/signup",
+            "/products/**",
+            "/categories/**"
+    };
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.requestMatchers(WhiteList).permitAll()
+                .requestMatchers("/user/**").hasRole("USER")
+                .requestMatchers("/admin/**").hasRole("ADMIN").anyRequest().authenticated());
+
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler));
+
+        http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()));
+
+        http.csrf(csrf -> csrf.disable());
+
+        http.addFilterBefore(authenticationJwtTokenFilter(),
+                UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    /*
+     * @Bean
+     * public CommandLineRunner initData(UserRepository repository, PasswordEncoder
+     * encoder) {
+     * return args -> {
+     * User user1 = new User();
+     * user1.setUsername("admin@email.com");
+     * user1.setEmail("admin@email.com");
+     * user1.setPassword(encoder.encode("1234"));
+     * user1.setRole("admin");
+     * user1.setEnabled(true);
+     * repository.save(user1);
+     * 
+     * User user2 = new User();
+     * user2.setUsername("user@email.com");
+     * user2.setEmail("user@email.com");
+     * user2.setPassword(encoder.encode("1234"));
+     * user2.setRole("user");
+     * user2.setEnabled(true);
+     * repository.save(user2);
+     * };
+     * }
+     */
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception {
+        return builder.getAuthenticationManager();
+    }
+
+}
