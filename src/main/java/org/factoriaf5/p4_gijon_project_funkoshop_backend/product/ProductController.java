4e806f57525c02d06ee5c3a2b1b9853b3200343b
andresvaz89 <andres.vazquez.martin@gmail.com>
2024-12-17 12:54:44 +0100

    changed endpoints for discount and stock to make it more readable, also tested on POSTMAN

--- src/main/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/product/ProductController.java
+++ src/main/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/product/ProductController.java
@@ -1,5 +1,6 @@
 package org.factoriaf5.p4_gijon_project_funkoshop_backend.product;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,16 +29,23 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
-@PostMapping
-public ResponseEntity<ProductDTO> addProduct(
-        @RequestPart("product") ProductDTO productDTO,
-        @RequestPart(value = "image1", required = false) MultipartFile image1,
-        @RequestPart(value = "image2", required = false) MultipartFile image2) throws Exception {
 
-    ProductDTO createdProduct = productService.createProduct(productDTO, image1, image2);
-    return ResponseEntity.ok(createdProduct);
-}
-@PutMapping("/{id}")
+    @PostMapping
+    public ResponseEntity<?> createProduct(
+            @ModelAttribute ProductDTO productDTO,
+            @RequestParam(value = "image1") MultipartFile image1,
+            @RequestParam(value = "image2") MultipartFile image2) {
+        try {
+            ProductDTO savedProduct = productService.createProduct(productDTO, image1, image2);
+            return ResponseEntity.ok(savedProduct);
+        } catch (IOException e) {
+            return ResponseEntity.status(500).body("Error saving product: " + e.getMessage());
+        }
+    }
+
+
+
+   /*  @PutMapping("/{id}")
     public ResponseEntity<ProductDTO> updateProduct(
             @PathVariable Long id,
             @RequestPart("product") ProductDTO productDTO,
@@ -47,11 +56,11 @@ public ResponseEntity<ProductDTO> addProduct(
         return ResponseEntity.ok(updatedProduct);
     }
     
-    @PatchMapping("/{id}/discount")
+    @PatchMapping("/discount")
     public ResponseEntity<ProductDTO> applyDiscountToProduct(@PathVariable Long id, @RequestBody Integer discount) {
         ProductDTO updatedProduct = productService.applyDiscount(id, discount);
         return ResponseEntity.ok(updatedProduct);
-    }
+    } */
     
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
@@ -108,7 +117,7 @@ public ResponseEntity<ProductDTO> addProduct(
 
     }
 
-    @PatchMapping("/{id}/stock")
+    @PatchMapping("/stock/{id}")
     public ResponseEntity<ProductDTO> updateProductStock(
         @PathVariable Long id,
         @RequestParam int quantity) {
