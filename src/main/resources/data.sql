4b59e21a75d557094afd1b62cbcc3c8b7be5374f
Maria Garcia <mgarciasierragrsr@gmail.com>
2024-12-17 09:23:09 +0100

    Updated dataSql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,10 @@
--- Insertar 3 productos en la tabla `products`
+INSERT INTO categories (id, name, image_hash, highlights) VALUES
+    (1, 'Marvel', 'X''FFD8FF''', true),
+    (2, 'Iron Man', 'X''FFD8FF''', false),
+    (3, 'Star Wars', 'X''FFD8FF''', true);
+
 INSERT INTO products (name, description, price, stock, discount, image_hash, image_hash2, category_id, created_at)
-VALUES 
+VALUES
     ('Funko Pop Spider-Man', 'Figura coleccionable de Spider-Man', 19.99, 50, 10, X'FFD8FF', X'FFD8FF', 1, '2024-12-16 12:00:00'),
     ('Funko Pop Iron Man', 'Figura coleccionable de Iron Man', 24.99, 30, 5, X'FFD8FF', X'FFD8FF', 2, '2024-12-16 12:05:00'),
     ('Funko Pop Darth Vader', 'Figura coleccionable de Darth Vader', 29.99, 20, 15, X'FFD8FF', X'FFD8FF', 3, '2024-12-16 12:10:00');
