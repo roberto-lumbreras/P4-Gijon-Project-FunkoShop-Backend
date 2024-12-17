46809131b05fb700509157b5149230a9d9584d58
AnaBHernandez <[anahernandezfit@gmail.com]>
2024-12-17 12:38:40 +0100

    Receiving changes

--- src/test/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/product/ProductControllerTest.java
+++ src/test/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/product/ProductControllerTest.java
@@ -27,7 +27,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters= false)
 class ProductControllerTest {
 
     @Autowired
