9c7766876fb5da4038fd47290dee98891bebfa11
Andres Vazquez <andres.vazquez.martin@gmail.com>
2024-12-18 10:11:49 +0100

    fixed commented SQL dta.sql file to avoid conflicts in coming merge with dev

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -23,7 +23,7 @@ CREATE TABLE products (
 ); */
 
 
-/* INSERT INTO categories (name, img_url, highlights) VALUES 
+INSERT INTO categories (name, img_url, highlights) VALUES 
 ('Marvel', 'aasdf', true), 
 ('Iron Man', 'aasdf', false), 
 ('Star Wars', 'aasdf', true);
@@ -34,4 +34,3 @@ VALUES
     ('Funko Pop Spider-Man', 'Figura coleccionable de Spider-Man', 19.99, 50, 10.00, 'aasdf', 'aasdf', 1, '2024-12-16 12:00:00'),
     ('Funko Pop Iron Man', 'Figura coleccionable de Iron Man', 24.99, 30, 5.00, 'aasdf', 'aasdf', 2, '2024-12-16 12:05:00'),
     ('Funko Pop Darth Vader', 'Figura coleccionable de Darth Vader', 29.99, 20, 15.00, 'aasdf', 'aasdf', 3, '2024-12-16 12:10:00');
- */
