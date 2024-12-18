700c2b7497b124d231898324ad63c3d8687b8c77
Acacia-Sanchez-Xanina <43806224+Acacia-Sanchez@users.noreply.github.com>
2024-12-18 13:42:44 +0100

    deleted updateProduct & made createdProductTest

--- src/main/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/product/ProductService.java
+++ src/main/java/org/factoriaf5/p4_gijon_project_funkoshop_backend/product/ProductService.java
@@ -21,10 +21,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class ProductService {
-
     @Autowired
     private ProductRepository productRepository;
-
     @Autowired
     private CategoryRepository categoryRepository;
 
@@ -33,99 +31,76 @@ public class ProductService {
                 .orElseThrow(() -> new RuntimeException("Product not found with id. Status: 404 NOT_FOUND " + id));
         productRepository.deleteById(id);
     }
-    
-private void imageDeletionService(String imgUrl) {
-        imgUrl = System.getProperty("user.dir")+"/src/main/resources/static"+imgUrl;
+
+    private void imageDeletionService(String imgUrl) {
+        imgUrl = System.getProperty("user.dir") + "/src/main/resources/static" + imgUrl;
         File img = new File(imgUrl);
-        if(img.exists()&&!imgUrl.contains("default")){
+        if (img.exists() && !imgUrl.contains("default")) {
             img.delete();
         }
     }
 
-
-    public ProductDTO createProduct(ProductDTO productDTO, MultipartFile image1, MultipartFile image2) throws IOException {
+    public ProductDTO createProduct(ProductDTO productDTO, MultipartFile image1, MultipartFile image2)
+            throws IOException {
         Category category = categoryRepository.findById(productDTO.getCategoryId())
-                .orElseThrow(() -> new RuntimeException("Category not found with id. Status: 404 NOT_FOUND " + productDTO.getCategoryId()));
-    
+                .orElseThrow(() -> new RuntimeException(
+                        "Category not found with id. Status: 404 NOT_FOUND " + productDTO.getCategoryId()));
         // Eliminamos 'product' y usamos directamente 'savedProduct'
         Product savedProduct = new Product();
-        //hace falta pasar id?
+        // hace falta pasar id?
         savedProduct.setName(productDTO.getName());
         savedProduct.setDescription(productDTO.getDescription());
         savedProduct.setPrice(productDTO.getPrice());
         savedProduct.setStock(productDTO.getStock());
         savedProduct.setCategory(category);
         savedProduct.setDiscount(productDTO.getDiscount());
-        if(!image1.isEmpty()){
-            if(savedProduct.getId()!=null){
+        if (!image1.isEmpty()) {
+            if (savedProduct.getId() != null) {
                 String url = fetchProductById(savedProduct.getId()).getImgUrl();
-                if(url!=null){
+                if (url != null) {
                     imageDeletionService(url);
                 }
             }
             savedProduct.setImgUrl(imageCreationService(image1));
-        }else if(savedProduct.getId()!=null){
+        } else if (savedProduct.getId() != null) {
             savedProduct.setImgUrl(fetchProductById(savedProduct.getId()).getImgUrl());
         }
-        if(!image2.isEmpty()){
-            if(savedProduct.getId()!=null){
+        if (!image2.isEmpty()) {
+            if (savedProduct.getId() != null) {
                 String url = fetchProductById(savedProduct.getId()).getImgUrl2();
-                if(url!=null){
+                if (url != null) {
                     imageDeletionService(url);
                 }
             }
             savedProduct.setImgUrl2(imageCreationService(image2));
-        }else if(savedProduct.getId()!=null){
+        } else if (savedProduct.getId() != null) {
             savedProduct.setImgUrl2(fetchProductById(savedProduct.getId()).getImgUrl2());
         }
         savedProduct.setCreatedAt(LocalDateTime.now());
-    
         // Guardamos el producto en la base de datos
         savedProduct = productRepository.save(savedProduct);
-    
         // Devolvemos el DTO con el producto guardado
-        return new ProductDTO(savedProduct);
+        if (savedProduct == null) {
+            throw new RuntimeException(
+                    "Failed to save the product to the database. Status: 500 INTERNAL SERVER ERROR.");
+        } else {
+            return new ProductDTO(savedProduct);
+        }
     }
-    
-    private String imageCreationService(MultipartFile img) throws IOException{
-        final String UPLOAD_DIR = System.getProperty("user.dir")+"/src/main/resources/static/images";
+
+    private String imageCreationService(MultipartFile img) throws IOException {
+        final String UPLOAD_DIR = System.getProperty("user.dir") + "/src/main/resources/static/images";
         File uploadDir = new File(UPLOAD_DIR);
-        System.out.println("ruta del directorio de carga de imagen -> "+uploadDir.getAbsolutePath());
-        if(!uploadDir.exists()){
+        System.out.println("ruta del directorio de carga de imagen -> " + uploadDir.getAbsolutePath());
+        if (!uploadDir.exists()) {
             uploadDir.mkdir();
         }
-        String filename = System.currentTimeMillis()+"_"+img.getOriginalFilename();
-    
-        uploadDir = new File(uploadDir,filename);
-        System.out.println("ruta del archivo de imagen -> "+uploadDir.getAbsolutePath());
+        String filename = System.currentTimeMillis() + "_" + img.getOriginalFilename();
+        uploadDir = new File(uploadDir, filename);
+        System.out.println("ruta del archivo de imagen -> " + uploadDir.getAbsolutePath());
         img.transferTo(uploadDir);
-        return "/images/"+filename;
+        return "/images/" + filename;
     }
-
-    /* public ProductDTO updateProduct(Long id, ProductDTO productDTO, MultipartFile image1, MultipartFile image2) throws IOException {
-        Product product = productRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Product not found with id " + id));
-
-        Category category = categoryRepository.findById(productDTO.getCategoryId())
-                .orElseThrow(() -> new RuntimeException("Category not found with id " + productDTO.getCategoryId()));
-
-        product.setName(productDTO.getName());
-        product.setDescription(productDTO.getDescription());
-        product.setPrice(productDTO.getPrice());
-        product.setStock(productDTO.getStock());
-        product.setDiscount(productDTO.getDiscount());
-        product.setCategory(category);
-
-        if (image1 != null) {
-            product.setImageHash(image1.getBytes());
-        }
-        if (image2 != null) {
-            product.setImageHash2(image2.getBytes());
-        }
-
-        Product updatedProduct = productRepository.save(product);
-        return new ProductDTO(updatedProduct);
-    } */
 
     public ProductDTO fetchProductById(Long id) {
         Product product = productRepository.findById(id)
@@ -141,14 +116,12 @@ private void imageDeletionService(String imgUrl) {
         Sort.Direction direction = Sort.Direction.valueOf(sortDirection);
         Pageable pageable = PageRequest.of(page, size, Sort.by(direction, sortField));
         Page<Product> productPage = productRepository.findAll(pageable);
-
         // Filtrar los productos después de obtener la página
         List<ProductDTO> filteredProducts = productPage.stream()
                 .filter(x -> x.getName().toLowerCase().contains(keyword.toLowerCase()) ||
                         x.getDescription().toLowerCase().contains(keyword.toLowerCase()))
                 .map(ProductDTO::new)
                 .collect(Collectors.toList());
-
         // Convertir la lista filtrada a una página
         return new PageImpl<>(filteredProducts, pageable, productPage.getTotalElements());
     }
